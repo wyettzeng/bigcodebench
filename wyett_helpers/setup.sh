e4e2abf69e2405619d01cbc8e364ccf2cd858e55
wyett <wyettzeng@gmail.com>
2024-11-14 20:56:11 -0500

    updates

---
+++ wyett_helpers/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+conda create -n bigcodebench python=3.10
+conda init
+conda activate bigcodebench
+
+# By default, you will use the remote evaluation API to execute the output samples.
+pip install bigcodebench --upgrade
+
+# You are suggested to use `flash-attn` for generating code samples.
+pip install packaging ninja
+pip install flash-attn --no-build-isolation
+# Note: if you have installation problem, consider using pre-built
+# wheels from https://github.com/Dao-AILab/flash-attention/releases
