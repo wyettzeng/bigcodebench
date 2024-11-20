727d9bd1ba1155aad39ae5e410b6c965bbbca7a7
wyett <wyettzeng@gmail.com>
2024-11-20 17:52:12 -0500

    updates

---
+++ myEvaluate/myEvaluate.py
@@ -0,0 +1,45 @@
+from fire import Fire
+from bigcodebench.evaluate import evaluate
+from bigcodebench.generate import run_codegen
+
+def main(
+        model: str,
+        split: str ="instruct",
+        subset:str = "hard",
+        n_samples: int= 16,
+        do_eval: bool = False
+    ):
+    if not do_eval:
+        print("Here")
+        if n_samples == 1:
+            # greedy we can just
+            run_codegen(
+                split=split,
+                subset=subset,
+                n_samples=n_samples,
+                model=model,
+                backend="vllm",
+            )
+        else:
+            run_codegen(
+                split=split,
+                subset=subset,
+                n_samples=n_samples,
+                model=model,
+                backend="vllm",
+                temperature=1.0
+            )
+    else:
+        revision: str = "main"
+        extra = "-" + subset if subset != "full" else ""
+        backend = "vllm"
+        temperature = 0 if n_samples == 1 else 1.0
+        identifier = model.replace("/", "--") + f"--{revision}--bigcodebench{extra}-{split}--{backend}-{temperature}-{n_samples}-sanitized_calibrated.jsonl"
+        inf_path = "bcb_results/" + identifier
+
+        evaluate(samples=inf_path, split=split, subset=subset, 
+                 local_execute=True
+                 )
+
+if __name__ == "__main__":
+    Fire(main)
--- myEvaluate/myEvaluate.sh
+++ myEvaluate/myEvaluate.sh
@@ -2,25 +2,50 @@
 
 models=(
   "Qwen/CodeQwen1.5-7B-Chat"
-  "Qwen/Qwen2.5-Coder-7B-Instruct"
-  "NTQAI/Nxcode-CQ-7B-orpo"
-  "meta-llama/Meta-Llama-3-8B-Instruct"
+  # "Qwen/Qwen2.5-Coder-7B-Instruct"
+  # "NTQAI/Nxcode-CQ-7B-orpo"
+  # "meta-llama/Meta-Llama-3-8B-Instruct"
+)
+
+splits=(
+  # "complete"
+  "instruct"
+)
+
+subsets=(
+  "hard"
+  # "full"
+)
+
+samples=(
+  # 1
+  16
 )
 
 for model in ${models[@]}
 do
-  bigcodebench.evaluate \
-    --model ${model} \
-    --split instruct \
-    --subset full \
-    --backend vllm
+  for split in ${splits[@]}
+  do
+    for subset in ${subsets[@]}
+    do
+      for sample in ${samples[@]}
+      do
+        # generate
+        # python myEvaluate/myEvaluate.py \
+        #   --model ${model} \
+        #   --split $split \
+        #   --subset $subset \
+        #   --n_samples $sample \
+        #   --do_eval False 
 
-  # best of N
-  bigcodebench.evaluate \
-    --model ${model} \
-    --split instruct \
-    --subset full \
-    --backend vllm \
-    --n_samples 16 \
-    --temperature 1.0
+        #eval
+        python myEvaluate/myEvaluate.py \
+          --model ${model} \
+          --split $split \
+          --subset $subset \
+          --n_samples $sample \
+          --do_eval True 
+      done
+    done
+  done
 done
