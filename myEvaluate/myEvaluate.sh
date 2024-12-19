01afc0614ebbdc513722e5a5d39ce662f38c2feb
wyett <wyettzeng@gmail.com>
2024-12-18 21:35:41 -0500

    updates

--- myEvaluate/myEvaluate.sh
+++ myEvaluate/myEvaluate.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 models=(
+  "codellama/CodeLlama-7b-Instruct-hf"
+  "mistralai/Mistral-7B-Instruct-v0.3"
   "Qwen/CodeQwen1.5-7B-Chat"
   "Qwen/Qwen2.5-Coder-7B-Instruct"
   "NTQAI/Nxcode-CQ-7B-orpo"
   "meta-llama/Meta-Llama-3-8B-Instruct"
+  "meta-llama/Llama-3.1-8B-Instruct"
 )
 
 splits=(
