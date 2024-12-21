f24123cbf209187bd3bb6d9f61abe920f3b42d25
wyett-ping <wyettzeng@gmail.com>
2024-12-21 00:36:49 +0000

    updates

--- myEvaluate/myEvaluate.sh
+++ myEvaluate/myEvaluate.sh
@@ -3,11 +3,11 @@
 models=(
   "codellama/CodeLlama-7b-Instruct-hf"
   "mistralai/Mistral-7B-Instruct-v0.3"
-  "Qwen/CodeQwen1.5-7B-Chat"
-  "Qwen/Qwen2.5-Coder-7B-Instruct"
-  "NTQAI/Nxcode-CQ-7B-orpo"
-  "meta-llama/Meta-Llama-3-8B-Instruct"
   "meta-llama/Llama-3.1-8B-Instruct"
+  "Qwen/CodeQwen1.5-7B-Chat"
+  # "Qwen/Qwen2.5-Coder-7B-Instruct"
+  # "NTQAI/Nxcode-CQ-7B-orpo"
+  # "meta-llama/Meta-Llama-3-8B-Instruct"
 )
 
 splits=(
