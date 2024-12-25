8408fed910ce315c3751e9e658eeb286d26e22c3
wyett <wyettzeng@gmail.com>
2024-12-24 21:48:26 -0500

    updates

---
+++ myEvaluate/rl_evaluate.py
@@ -0,0 +1,30 @@
+from fire import Fire
+from bigcodebench.evaluate import evaluate
+from bigcodebench.generate import run_codegen
+import os
+def main(
+        model: str,
+        split: str ="instruct",
+        subset:str = "hard",
+    ):
+    print(f"Starting {model} - {split} - {subset}")
+    revision: str = "main"
+    extra = "-" + subset if subset != "full" else ""
+    backend = "vllm"
+    temperature = 0
+
+    # we evaluate locally as it errored out with API
+    out_file_name = model.replace("/", "--") + f"--{revision}--bigcodebench{extra}-{split}--{backend}-{temperature}-1-sanitized_calibrated_eval_results.json"
+    out_file_path = "haozhe/" + out_file_name
+
+    if os.path.exists(out_file_path):
+        return # already done
+
+    identifier = model.replace("/", "--") + f"--{revision}--bigcodebench{extra}-{split}--{backend}-{temperature}-1-sanitized_calibrated.jsonl"
+    inf_path = "haozhe/" + identifier
+    evaluate(samples=inf_path, split=split, subset=subset, 
+                # local_execute=True
+                )
+
+if __name__ == "__main__":
+    Fire(main)
---
+++ myEvaluate/rl_evaluate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+models=(
+  # "codellama/CodeLlama-7b-Instruct-hf"
+  # "mistralai/Mistral-7B-Instruct-v0.3"
+  # "meta-llama/Llama-3.1-8B-Instruct"
+  # "Qwen/CodeQwen1.5-7B-Chat"
+  "Qwen/Qwen2.5-Coder-7B-Instruct"
+  # "NTQAI/Nxcode-CQ-7B-orpo"
+  # "meta-llama/Meta-Llama-3-8B-Instruct"
+)
+
+splits=(
+  "complete"
+  "instruct"
+)
+
+subsets=(
+  "hard"
+  "full"
+)
+
+
+for model in ${models[@]}
+do
+  for split in ${splits[@]}
+  do
+    for subset in ${subsets[@]}
+    do
+        python myEvaluate/rl_evaluate.py \
+          --model ${model} \
+          --split $split \
+          --subset $subset \
+    done
+  done
+done
