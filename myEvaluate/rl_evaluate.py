0eb5f7e7cea382abaeee74c013decb9bfd30ee4f
wyett <wyettzeng@gmail.com>
2025-01-26 10:02:02 -0500

    updates

--- myEvaluate/rl_evaluate.py
+++ myEvaluate/rl_evaluate.py
@@ -1,30 +1,35 @@
 from fire import Fire
 from bigcodebench.evaluate import evaluate
-from bigcodebench.generate import run_codegen
+
 import os
 def main(
         model: str,
         split: str ="instruct",
         subset:str = "hard",
+        **generation_kwargs,
     ):
-    print(f"Starting {model} - {split} - {subset}")
     revision: str = "main"
     extra = "-" + subset if subset != "full" else ""
     backend = "vllm"
     temperature = 0
-
-    # we evaluate locally as it errored out with API
-    out_file_name = f"{model}--{revision}--bigcodebench{extra}-{split}--{backend}-{temperature}-1-sanitized_calibrated_eval_results.json"
-    out_file_path = "haozhe/" + out_file_name
+    out_file_name = model.replace("/", "--") + f"--{revision}--bigcodebench{extra}-{split}--{backend}-{temperature}-sanitized_calibrated_eval_results.json"
+    out_file_path = "bcb_results/" + out_file_name
 
     if os.path.exists(out_file_path):
+        print(f"Already finished {model} - {split} - {subset}")
         return # already done
+    
+    print(f"Starting {model} - {split} - {subset}")
+    evaluate(
+        split=split,
+        subset=subset,
+        n_samples=1,
+        model=model,
+        backend="vllm",
+        # local_execute=True,
+        **generation_kwargs,
+    )
 
-    identifier = f"{model}--{revision}--bigcodebench{extra}-{split}--{backend}-{temperature}-1-sanitized_calibrated.jsonl"
-    inf_path = "haozhe/inference/" + identifier
-    evaluate(samples=inf_path, split=split, subset=subset, 
-                # local_execute=True
-                )
 
 if __name__ == "__main__":
     Fire(main)
