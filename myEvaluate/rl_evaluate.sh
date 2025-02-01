30153d772e962252bddf9cd4cf7d641184d568d6
wyettzeng <wyettzeng@gmail.com>
2025-02-01 22:28:56 +0000

    updates

--- myEvaluate/rl_evaluate.sh
+++ myEvaluate/rl_evaluate.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 # Available GPUs
-cuda_devices=(1 4)
+cuda_devices=(4 5 6 7)
 
 models=(
-  # "/data/code_llm/trained/qwen25-coder-7b-testcasermbinaryFalse-7b-reinforcepp_new_dataset_hard"
-  # "/data/code_llm/trained/qwen25-coder-7b-testcasermbinaryTrue-7b-reinforcepp_new_dataset_hard-binary"
-  # "CodeDPO/qwen25-coder-base-7b-testcaserm-7b-new-dataset-hard"
-  "/data/code_llm/trained/qwen25-coder-7b-rm-7b-reinforcepp_new_dataset_hard"
-  "/data/code_llm/trained/qwen25-base-7b-rm-7b-reinforcepp_new_dataset_hard"
-  "CodeDPO/qwen25-ins-7b-coderm_new_margin_scalebt-7b-reinforce-plus-episode_1"
+  "CodeDPO/qwen25-coder-ins-7b-coderm_new_sigmoid-c7b-reinforce-plus"
 )
 
-# model="trained_models/lr10_w_E1_kl0.5_CodeRLTrain_llama3_IPS_max2048_____E1"
 
 splits=(
   "complete"
