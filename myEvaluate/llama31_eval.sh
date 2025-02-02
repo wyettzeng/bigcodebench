82661f9be802f741490146fc24c00a5340529933
wyett <wyettzeng@gmail.com>
2025-02-02 01:23:06 -0500

    udpates

--- myEvaluate/llama31_eval.sh
+++ myEvaluate/llama31_eval.sh
@@ -13,30 +13,11 @@ subsets=(
   "full"
 )
 
-n_lst=(
-  # 16
-  # 32
-  64
-)
-
-for n in "${n_lst[@]}"
-do
-    # Inside this process, loop over splits and subsets
-    for split in "${splits[@]}"
-    do
-      for subset in "${subsets[@]}"
-      do
-        # You can adjust or uncomment your commands here as needed.
-        # For example, if you only want to do eval, do:
-        python myEvaluate/myEvaluate.py \
-          --model "${model}" \
-          --split "${split}" \
-          --subset "${subset}" \
-          --n_samples "${n}" \
-          --do_eval True &
-      done
-    done
-done
-
+python myEvaluate/myEvaluate.py \
+  --model "${model}" \
+  --split "instruct" \
+  --subset "full" \
+  --n_samples 64 \
+  --do_eval True
 
 wait
